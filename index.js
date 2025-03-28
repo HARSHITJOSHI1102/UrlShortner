3a922d6765d0794d8a2746a53e6e45a447add1de
Harshit <harshitjoshi1102@gmail.com>
2025-03-29 00:13:29 +0530

    Latest

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const app = express();
+const path = require('path')
 const { connectToMongoDB } = require('./connect')
 const urlRoute = require("./routes/url")
+const staticRoute = require("./routes/staticRouter")
 const URL = require('./models/url')
-const app = express();
 const PORT = 8001;
 connectToMongoDB("mongodb://localhost:27017/short-url").then(() => console.log("MongoDb Connected"));
+
+app.set("view engine" , "ejs")
+app.set("views" , path.resolve("./views"))
 app.use(express.json())
+
+app.get("/test" , async(req,res)=>{
+    const allUrls = await URL.find({})
+    return res.render("home",{
+        urls:allUrls,
+        name:"Piyush"
+    })
+});
+
 app.use("/url", urlRoute)
 app.get('/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
@@ -16,6 +30,9 @@ app.get('/:shortId', async (req, res) => {
             visitHistory: { timestamp: Date.now(), },
         }
     })
+    if(!entry){
+        return res.json("Unable to fetch");
+    }
     res.redirect(entry.redirectURL)
 })
 app.listen(PORT, () => console.log(`Server Started at PORT : ${PORT}`))
