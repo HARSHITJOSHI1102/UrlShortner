1c4938abe95a43a7fb5325dbe70a67d097d09f45
Harshit <harshitjoshi1102@gmail.com>
2025-03-30 22:49:44 +0530

    Authentication

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const { connectToMongoDB } = require('./connect')
-const urlRoute = require("./routes/url")
-const staticRoute = require("./routes/staticRouter")
+const {restrictToLoggedinUserOnly,checkAuth} = require("./middlewares/auth")
 const URL = require('./models/url')
 const PORT = 8001;
 connectToMongoDB("mongodb://localhost:27017/short-url").then(() => console.log("MongoDb Connected"));
 
-app.set("view engine" , "ejs")
-app.set("views" , path.resolve("./views"))
+const urlRoute = require("./routes/url")
+const staticRoute = require("./routes/staticRouter")
+const userRoute = require("./routes/user");
+const cookieParser = require('cookie-parser');
+app.set("view engine", "ejs")
+app.set("views", path.resolve("./views"))
 app.use(express.json())
-app.use(express.urlencoded({extended : false}))
-app.use("/url" , urlRoute)
-app.use("/",staticRoute)
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser());
+app.use("/url",restrictToLoggedinUserOnly, urlRoute)
+app.use("/",checkAuth, staticRoute)
+app.use("/user", userRoute);
+
+
 
 app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
@@ -24,7 +31,7 @@ app.get('/url/:shortId', async (req, res) => {
             visitHistory: { timestamp: Date.now(), },
         }
     })
-    if(!entry){
+    if (!entry) {
         return res.json("Unable to fetch");
     }
     res.redirect(entry.redirectURL)
